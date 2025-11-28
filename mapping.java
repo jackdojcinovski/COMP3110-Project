156d3838e63012c809393c6838736dbd9d115b94
oconnorc10 <ciaranoconnor1010@gmail.com>
2025-11-28 13:17:37 -0500

    3

---
+++ mapping.java
@@ -0,0 +1,15 @@
+public class mapping {
+    /*load old file
+load new file
+
+compare line by line
+detect matches
+detect insertions
+detect deletions
+
+produce mapping structure:
+{ old_line_num : new_line_num or -1 }
+ */
+
+
+}
