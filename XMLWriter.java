9f30688c992ab566e7cea26e3c7fd3be8035cf59
oconnorc10 <ciaranoconnor1010@gmail.com>
2025-12-03 14:06:58 -0500

    Add comments to XMLWriter

--- XMLWriter.java
+++ XMLWriter.java
@@ -4,12 +4,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class XMLWriter {
+    //Writes line mapping to XML structure
 
     public static void writeXml(String testName, Path output, MappingResult result) throws IOException {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("<TEST NAME=\"%s\">\n", testName));
-        sb.append("  <VERSION NUMBER=\"1\" CHECKED=\"TRUE\">\n");
-
+        sb.append("  <VERSION NUMBER=\"1\" CHECKED=\"TRUE\">\n"); //Always add
+        
+        //Each mapping entry as XML tag, ORIg is old file line number, NEW is corresponding line number in new file
         for (Map.Entry<Integer, Integer> e : result.getMapping().entrySet()) {
             sb.append(String.format("    <LOCATION ORIG=\"%d\" NEW=\"%d\"/>\n",
                     e.getKey(), e.getValue()));
