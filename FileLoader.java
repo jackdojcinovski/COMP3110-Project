d271a4cb3836a6e9df6c0d57e9c4e52b096cf93f
oconnorc10 <ciaranoconnor1010@gmail.com>
2025-12-03 14:06:10 -0500

    Add Comments to FIleLoader

--- FileLoader.java
+++ FileLoader.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.*;
 
 public class FileLoader {
-
+    //Reads all lines from a file and trims whitespace
     public static List<String> loadLines(Path path, boolean normalizeWhitespace) throws IOException {
         List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 
-        if (!normalizeWhitespace) return lines;
-
+        if (!normalizeWhitespace) return lines;//Base Case
+        //Remove leading/trailing whitespace from each line
         List<String> normalized = new ArrayList<>();
         for (String line : lines) {
             normalized.add(line.strip());
