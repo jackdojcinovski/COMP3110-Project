6f83856d0139f5c56d3800d3431e12935b44c132
oconnorc10 <ciaranoconnor1010@gmail.com>
2025-12-03 14:06:46 -0500

    Add comments to MappingResult

--- MappingResult.java
+++ MappingResult.java
@@ -1,14 +1,18 @@
 import java.util.*;
 
 public class MappingResult {
+   
+    //Store mappings of oldLine -> newLine
+
     private final Map<Integer, Integer> mapping = new LinkedHashMap<>();
 
     public MappingResult(int oldLineCount) {
+        //Initialize every old line as -1
         for (int i = 1; i <= oldLineCount; i++) {
             mapping.put(i, -1);
         }
     }
-
+    //For a matched pair
     public void addMatch(int oldLine, int newLine) {
         mapping.put(oldLine, newLine);
     }
